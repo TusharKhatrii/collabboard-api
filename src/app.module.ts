1d20975102b40bdf38a61bb3faa20aa6f9b9f70d
TusharKhatrii <tusharkhatrii05@gmail.com>
2026-08-29 21:04:26 +0500

    initialize NestJS app with MongoDB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes env vars available everywhere without re-importing
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
